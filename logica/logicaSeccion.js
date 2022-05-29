55eb3471fc6e251c92778bae4324841eb6623a27
hbonilla6 <106295995+hbonilla6@users.noreply.github.com>
2022-05-29 16:37:35 -0600

    contenido de seccion

--- logica/logicaSeccion.js
+++ logica/logicaSeccion.js
@@ -1,26 +1,13 @@
 let datos = "https://rickandmortyapi.com/api/character";
-
-function mostrar(data) {
- /*  document.getElementById("nombre").innerText = data[0].name;
-  document.getElementById("imagen").src = data[0].image;
- */
-  let da = data[0];
-
-  let claves = Object.keys(da)
-
-  /* for(const value in da){
-      console.log(da[value]);
-      document.getElementById("datos").innerText = value + " " + da[value];
-  } */
-  for (let index = 0; index < claves.length; index++) {
-      const element = claves[index];
-      
-      console.log(da[element])
-      document.getElementById("datos").innerHTML = da[element];
-  }
-
-}
-
 fetch(datos)
-  .then((response) => response.json())
-  .then((data) => mostrar(data.results));
+	.then((response) => response.json())
+	.then((data) => mostrar(data.results));
+
+  function mostrar(data) {
+  let dat = Object.values(data[0]);
+  var foo = dat.map(function(bar){
+    return `<li>${bar}</li>`
+  })
+  
+	document.getElementById("datos").innerHTML = foo;
+}
