145f3ddf75d5350b355a973bbc32e86d2f7f27b1
hbonilla6 <106295995+hbonilla6@users.noreply.github.com>
2022-06-01 23:55:57 -0600

    contenido seccion

--- logica/logicaSeccion.js
+++ logica/logicaSeccion.js
@@ -1,13 +1,67 @@
 let datos = "https://rickandmortyapi.com/api/character";
 fetch(datos)
-	.then((response) => response.json())
-	.then((data) => mostrar(data.results));
-
-  function mostrar(data) {
-  let dat = Object.values(data[0]);
-  var foo = dat.map(function(bar){
-    return `<li>${bar}</li>`
-  })
+  .then((response) => response.json())
+  .then((data) => cambiar(data.results));
   
-	document.getElementById("datos").innerHTML = foo;
+  
+
+
+  
+  var x = 0;
+
+  function sumar (x, data){
+    if(x === 0){
+
+      console.log( data[0].name, x)
+    }
+    else if(x === 1){
+      console.log( data?.[1].name, x)
+
+    }
+    
+    
+    // console.log();
+  
+    // console.log(x, data[x].id)
+  
+    // document.getElementById('cont').innerText = x
+    
+    
+  //   for (let index = 0; index < data.length; index++) {
+  //     // let cl = Object.values(data[index].id);
+  //     // const element = `${index}${data}`;
+  // }
+  
+  
+  // console.log(data);
+  
+  
+  
+  
+  
+  // console.log(data)
+  // if (data !== undefined) {
+  
+  
+  
+  
+  
+  //   console.log(cl);
+  //   var foo = cl.map(function (bar) {
+  //     return `<li>${bar}</li>`
+  //   })
+  //   document.getElementById("datos").innerHTML = `${foo.join("")}</li>`;
+  
+  // }
+  // console.log(cl.length);
+
+  }  
+
+
+  
+  function cambiar(data) {
+    sumar(x++, data)
+
+    
+
 }
